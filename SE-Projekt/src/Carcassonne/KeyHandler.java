96e008a3c9b91ce60c7aafb100db4eff0d430fc9
Noricia <Noricia@192.168.0.3>
2021-12-09 14:29:30 +0100

    First Commit

---
+++ SE-Projekt/src/Carcassonne/KeyHandler.java
@@ -0,0 +1,78 @@
+package Carcassonne;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.MouseInputListener;
+
+public class KeyHandler implements KeyListener, MouseInputListener, ActionListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		vc.offsetX += (vc.moveMod * (e.getKeyCode() == 'A' ? 1 : 0)) - (vc.moveMod * (e.getKeyCode() == 'D' ? 1 : 0));
+		vc.offsetY += (vc.moveMod * (e.getKeyCode() == 'W' ? 1 : 0)) - (vc.moveMod * (e.getKeyCode() == 'S' ? 1 : 0));
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+}
