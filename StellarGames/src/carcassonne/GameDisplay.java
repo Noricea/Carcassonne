4680dcd24b06b66d253283bf1343f5da059e8789
Noricia <nori-matschuck-o@web.de>
2022-06-13 12:46:51 +0200

    CC adjusted

--- StellarGames/src/carcassonne/GameDisplay.java
+++ StellarGames/src/carcassonne/GameDisplay.java
@@ -23,7 +23,7 @@ import javax.swing.JLabel;
 public class GameDisplay extends JLabel {
 	private static final long serialVersionUID = 1L;
 
-	List<BufferedImage> Image = new ArrayList<BufferedImage>();
+	List<BufferedImage> imageList = new ArrayList<BufferedImage>();
 	List<TileSet> tileMap = new ArrayList<TileSet>();
 	int cursorTile;
 
@@ -37,7 +37,7 @@ public class GameDisplay extends JLabel {
 	GameDisplay() {
 		try {
 			for (int i = 0; i <= 12; i++) {
-				Image.add(ImageIO.read(new File("./src/sprites/" + i + ".png")));
+				imageList.add(ImageIO.read(new File("./src/sprites/" + i + ".png")));
 			}
 		} catch (IOException e) {
 
@@ -51,36 +51,36 @@ public class GameDisplay extends JLabel {
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		for (int i = 0; i < tileMap.size(); i++) {
-			if (Image.size() > 0) {
-				g.drawImage(Image.get(0), (tileMap.get(i).getX() * Image.get(0).getHeight()) + positionOnMap.x,
-						(tileMap.get(i).getY() * Image.get(0).getWidth()) + positionOnMap.y, null);
+			if (imageList.size() > 0) {
+				g.drawImage(imageList.get(0), (tileMap.get(i).getX() * imageList.get(0).getHeight()) + positionOnMap.x,
+						(tileMap.get(i).getY() * imageList.get(0).getWidth()) + positionOnMap.y, null);
 
 				if (tileMap.get(i).getId() / 1000 != 0) {
-					g.drawImage(Image.get(tileMap.get(i).getId() / 1000 * 4 - 3),
-							(tileMap.get(i).getX() * Image.get(0).getHeight()) + positionOnMap.x,
-							(tileMap.get(i).getY() * Image.get(0).getWidth()) + positionOnMap.y, null);
+					g.drawImage(imageList.get(tileMap.get(i).getId() / 1000 * 4 - 3),
+							(tileMap.get(i).getX() * imageList.get(0).getHeight()) + positionOnMap.x,
+							(tileMap.get(i).getY() * imageList.get(0).getWidth()) + positionOnMap.y, null);
 				}
 
 				if ((tileMap.get(i).getId() / 100) % 10 != 0) {
-					g.drawImage(Image.get((tileMap.get(i).getId() / 100) % 10 * 4 - 2),
-							(tileMap.get(i).getX() * Image.get(0).getHeight()) + positionOnMap.x + 32,
-							(tileMap.get(i).getY() * Image.get(0).getWidth()) + positionOnMap.y, null);
+					g.drawImage(imageList.get((tileMap.get(i).getId() / 100) % 10 * 4 - 2),
+							(tileMap.get(i).getX() * imageList.get(0).getHeight()) + positionOnMap.x + 32,
+							(tileMap.get(i).getY() * imageList.get(0).getWidth()) + positionOnMap.y, null);
 				}
 
 				if ((tileMap.get(i).getId() / 10) % 10 != 0) {
-					g.drawImage(Image.get((tileMap.get(i).getId() / 10) % 10 * 4 - 1),
-							(tileMap.get(i).getX() * Image.get(0).getHeight()) + positionOnMap.x,
-							(tileMap.get(i).getY() * Image.get(0).getWidth()) + positionOnMap.y + 32, null);
+					g.drawImage(imageList.get((tileMap.get(i).getId() / 10) % 10 * 4 - 1),
+							(tileMap.get(i).getX() * imageList.get(0).getHeight()) + positionOnMap.x,
+							(tileMap.get(i).getY() * imageList.get(0).getWidth()) + positionOnMap.y + 32, null);
 				}
 
 				if (tileMap.get(i).getId() % 10 != 0) {
-					g.drawImage(Image.get(tileMap.get(i).getId() % 10 * 4),
-							(tileMap.get(i).getX() * Image.get(0).getHeight()) + positionOnMap.x,
-							(tileMap.get(i).getY() * Image.get(0).getWidth()) + positionOnMap.y, null);
+					g.drawImage(imageList.get(tileMap.get(i).getId() % 10 * 4),
+							(tileMap.get(i).getX() * imageList.get(0).getHeight()) + positionOnMap.x,
+							(tileMap.get(i).getY() * imageList.get(0).getWidth()) + positionOnMap.y, null);
 				}
 			} else {
 				// Void
@@ -94,43 +94,43 @@ public class GameDisplay extends JLabel {
 		/**
 		 * Displays the Tile that the player can place.
 		 */
-		if (Image.size() > 0) {
-			g.drawImage(Image.get(0),
-					(positionOfCursor.x / Image.get(0).getHeight()) * Image.get(0).getHeight()
-							+ (positionOnMap.x % Image.get(0).getHeight()),
-					(positionOfCursor.y / Image.get(0).getWidth()) * Image.get(0).getWidth()
-							+ (positionOnMap.y % Image.get(0).getWidth()),
+		if (imageList.size() > 0) {
+			g.drawImage(imageList.get(0),
+					(positionOfCursor.x / imageList.get(0).getHeight()) * imageList.get(0).getHeight()
+							+ (positionOnMap.x % imageList.get(0).getHeight()),
+					(positionOfCursor.y / imageList.get(0).getWidth()) * imageList.get(0).getWidth()
+							+ (positionOnMap.y % imageList.get(0).getWidth()),
 					null);
 			if (cursorTile / 1000 != 0) {
-				g.drawImage(Image.get(cursorTile / 1000 * 4 - 3),
-						(positionOfCursor.x / Image.get(0).getHeight()) * Image.get(0).getHeight()
-								+ (positionOnMap.x % Image.get(0).getHeight()),
-						(positionOfCursor.y / Image.get(0).getWidth()) * Image.get(0).getWidth()
-								+ (positionOnMap.y % Image.get(0).getWidth()),
+				g.drawImage(imageList.get(cursorTile / 1000 * 4 - 3),
+						(positionOfCursor.x / imageList.get(0).getHeight()) * imageList.get(0).getHeight()
+								+ (positionOnMap.x % imageList.get(0).getHeight()),
+						(positionOfCursor.y / imageList.get(0).getWidth()) * imageList.get(0).getWidth()
+								+ (positionOnMap.y % imageList.get(0).getWidth()),
 						null);
 			}
 			if ((cursorTile / 100) % 10 != 0) {
-				g.drawImage(Image.get((cursorTile / 100) % 10 * 4 - 2),
-						(positionOfCursor.x / Image.get(0).getHeight()) * Image.get(0).getHeight()
-								+ (positionOnMap.x % Image.get(0).getHeight()) + 32,
-						(positionOfCursor.y / Image.get(0).getWidth()) * Image.get(0).getWidth()
-								+ (positionOnMap.y % Image.get(0).getWidth()),
+				g.drawImage(imageList.get((cursorTile / 100) % 10 * 4 - 2),
+						(positionOfCursor.x / imageList.get(0).getHeight()) * imageList.get(0).getHeight()
+								+ (positionOnMap.x % imageList.get(0).getHeight()) + 32,
+						(positionOfCursor.y / imageList.get(0).getWidth()) * imageList.get(0).getWidth()
+								+ (positionOnMap.y % imageList.get(0).getWidth()),
 						null);
 			}
 			if ((cursorTile / 10) % 10 != 0) {
-				g.drawImage(Image.get((cursorTile / 10) % 10 * 4 - 1),
-						(positionOfCursor.x / Image.get(0).getHeight()) * Image.get(0).getHeight()
-								+ (positionOnMap.x % Image.get(0).getHeight()),
-						(positionOfCursor.y / Image.get(0).getWidth()) * Image.get(0).getWidth()
-								+ (positionOnMap.y % Image.get(0).getWidth()) + 32,
+				g.drawImage(imageList.get((cursorTile / 10) % 10 * 4 - 1),
+						(positionOfCursor.x / imageList.get(0).getHeight()) * imageList.get(0).getHeight()
+								+ (positionOnMap.x % imageList.get(0).getHeight()),
+						(positionOfCursor.y / imageList.get(0).getWidth()) * imageList.get(0).getWidth()
+								+ (positionOnMap.y % imageList.get(0).getWidth()) + 32,
 						null);
 			}
 			if (cursorTile % 10 != 0) {
-				g.drawImage(Image.get(cursorTile % 10 * 4),
-						(positionOfCursor.x / Image.get(0).getHeight()) * Image.get(0).getHeight()
-								+ (positionOnMap.x % Image.get(0).getHeight()),
-						(positionOfCursor.y / Image.get(0).getWidth()) * Image.get(0).getWidth()
-								+ (positionOnMap.y % Image.get(0).getWidth()),
+				g.drawImage(imageList.get(cursorTile % 10 * 4),
+						(positionOfCursor.x / imageList.get(0).getHeight()) * imageList.get(0).getHeight()
+								+ (positionOnMap.x % imageList.get(0).getHeight()),
+						(positionOfCursor.y / imageList.get(0).getWidth()) * imageList.get(0).getWidth()
+								+ (positionOnMap.y % imageList.get(0).getWidth()),
 						null);
 			}
 		} else {
